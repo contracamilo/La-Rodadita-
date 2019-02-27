d99a56c160aad11822621fa9135826631ba80cbe
camilo rivera <camilo.riveraq@gmail.com>
2019-02-27 14:43:32 -0500

    creating cloud functions for notifications

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,43 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const createNotification = ((notification) => {
+  return admin.firestore().collection('notifications')
+    .add(notification)
+    .then(doc => console.log('notification added', doc));
+});
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello, darkness my old friend!");
+
+exports.tripCreated = functions.firestore
+  .document('trips/{tripId}')
+  .onCreate(doc => {
+
+    const trip = doc.data();
+    const notification = {
+      content: 'Añanadio un nuevo viaje',
+      user: `${trip.authorFirstName} ${trip.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    }
+
+    return createNotification(notification);
+
+});
+
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+    
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Se unio a la rodadita',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        };
+
+        return createNotification(notification);
+
+      });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -74,6 +74,6 @@ module.exports = {
   devServer: {
     contentBase: DIST_DIR,
     hot: true,
-    port: 9002
+    port: 9005
   }
 };
