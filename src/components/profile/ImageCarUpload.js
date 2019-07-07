7a55ee5e0f333f4a24d9ac6a6496d9d5d6c240b7
camilo rivera <camilo.riveraq@gmail.com>
2019-07-07 05:55:35 -0500

    uploading pictures

---
+++ src/components/profile/ImageCarUpload.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { uploadCarPic } from '../../store/actions/profileActions'
+
+class ImageCarUpload extends Component {
+    
+    state = {
+        file:null,
+        image: null,
+        url: ''
+    }
+    
+    handleChange = (e) => {
+
+        let value = e.target.value
+        let val =  value.substring(12, value.length).split('.').shift();
+       
+        if(e.target.files[0]) {
+            let image = e.target.files[0];
+            this.setState(() => ({image})) 
+        }
+
+        this.setState({
+            [e.target.id]: val
+        })
+    }
+
+
+    handleUpload = (e) => {
+        e.preventDefault();
+        const {file, image} = this.state
+        this.props.uploadCarPic(file.trim(), image)
+    }
+    
+    
+    render() {
+        return (
+            <div className="profile__input-file"> 
+                <p className="adv">Agrega una foto del auto: <span>Debe ser reciente y solo del auto.</span></p>
+                <form className="cnt">
+                    <div>
+                        <label htmlFor="file">Buscar</label>
+                        <input id="file" name="file"  type="file" onChange={this.handleChange}/>
+                    </div>    
+                    <div>
+                        <button className="btn" onClick={this.handleUpload}>Subir Foto</button>
+                    </div>
+                 </form>
+                 <div className="name">
+                    
+                        {
+                            (this.state.file) && <p> <strong> Archivo:</strong> {this.state.file}</p>
+                        }
+                       
+                 </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+   return {
+        uploadCarPic : (name, pic) => dispatch(uploadCarPic(name, pic))
+   }
+}
+
+
+export default connect(mapStateToProps ,mapDispatchToProps)(ImageCarUpload)
+
+
